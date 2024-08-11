b0ee0397ed1df6a7e528392bfd82d276f28cab29
Mike Depinet <mike.depinet@gmail.com>
2024-08-10 17:15:12 -0700

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System;
+
+namespace EasyTame
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Rune Factory 5.exe")]
+    public class Plugin : BasePlugin
+    {
+        internal static new ManualLogSource Log;
+        private Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log = base.Log;
+            Log.LogInfo($"Patching...");
+            harmony.PatchAll();
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+        }
+    }
+
+    [HarmonyPatch(typeof(Calc.CalcStatus), nameof(Calc.CalcStatus.CalcTame))]
+    public class CalcTamePatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
